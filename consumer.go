c8a66830b6b21462a6e65380a056b2bf61576588
Sean Brown <smbrown3706@gmail.com>
2016-09-20 15:13:28 -0400

    added a Consumer interface and Consumer struct that other consumers should inherit from

---
+++ consumer.go
@@ -0,0 +1,16 @@
+package crawler
+
+import "sync"
+
+type iConsumer interface {
+	Consume()
+}
+
+type Consumer struct {
+	/* implement the "Consumer" interface */
+	iConsumer
+	/* channel to tell the consumer when to quit */
+	quit chan int
+	/* waitGroup to signal to the main service when the consumer thread has stopped */
+	waitGroup *sync.WaitGroup
+}
