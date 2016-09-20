4d1ed862122ea2f4050204a087ce19c93a9d9b28
Sean Brown <smbrown3706@gmail.com>
2016-09-20 16:11:31 -0400

    fleshed out consume()

--- DataConsumer.go
+++ DataConsumer.go
@@ -2,7 +2,9 @@ package crawler
 
 import (
 	"sync"
-	//"log"
+	"log"
+	"regexp"
+	"github.com/PuerkitoBio/goquery"
 )
 
 type DataConsumer struct {
@@ -10,6 +12,10 @@ type DataConsumer struct {
 	Consumer
 	/* the channel of data that the consumer will parse */
 	data chan DataCollection
+	/* the rules for parsing the DOM */
+	rules []DOMParsingRule
+	/* a dependency-injected data storage object to persist the data */
+	storage DataStorage
 }
 /* Make a new Data consumer */
 func NewDataConsumer(data chan DataCollection, quit chan int) *DataConsumer {
@@ -25,26 +31,43 @@ func NewDataConsumer(data chan DataCollection, quit chan int) *DataConsumer {
 }
 
 /* rules for parsing data from the DOM */
-type DOMParsingRules struct {
-	tags []string
+type DOMParsingRule struct {
+	/* url pattern to match the rule to */
+	urlMatch string
+	/* the data goquery selector string (e.g. "div.content > div#main p.text") */
+	dataSelector string
 }
 
 /* Consumption Loop */
-//func (consumer *DataConsumer) Consume() {
-//	defer consumer.waitGroup.Done()
-//	for {
-//		select {
-//		case <-consumer.quit:
-//			log.Println("data consumer received the quit signal")
-//			break
-//		case data := <-consumer.data:
-//			log.Println("data consumer received data for ", data.url)
-//			consumer.consume(data)
-//		}
-//	}
-//}
+func (consumer *DataConsumer) Consume() {
+	defer consumer.waitGroup.Done()
+	for {
+		select {
+		case <-consumer.quit:
+			log.Println("data consumer received the quit signal")
+			break
+		case data := <-consumer.data:
+			log.Println("data consumer received data for ", data.url)
+			go consumer.consume(data)
+		}
+	}
+}
 
 /* Consume the data */
 func (consumer *DataConsumer) consume(data DataCollection) {
-	/* TODO consume the data */
+	/* iterate the DOM-parsing rules */
+	for _, rule := range consumer.rules {
+		/* check if this rule applies to this url */
+		matched, err := regexp.MatchString(rule.urlMatch, data.url)
+		if err != nil {
+			log.Println("Error matching url regex <", rule.urlMatch, "> with ", data.url)
+		} else if matched {
+			/* the rule does apply to this url, apply the rule */
+			log.Println("Matched <", rule.urlMatch, "> to ", data.url)
+			data.dom.Find(rule.dataSelector).Each(func (_ int, sel *goquery.Selection) {
+				/* store the data */
+				consumer.storage.Store(sel.Text())
+			})
+		}
+	}
 }
