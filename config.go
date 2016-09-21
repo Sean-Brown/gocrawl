97cde9f23c96fc5636c71cbb2a8a8313164b707e
Sean Brown <smbrown3706@gmail.com>
2016-09-21 10:27:24 -0400

    Fixed a bug where JSON parsing wasn't working due to the struct member names not being capitalized. Split ReadConfig into a function readConfig() that unmarshals a byte array

--- config.go
+++ config.go
@@ -6,42 +6,50 @@ import (
 	"encoding/json"
 )
 
-/* The configuration for crawling */
+/* The configuration for crawling. Note the field names need to start with capital
+letters in order for the JSON parser to not ignore the field (lower-case members are ignored) */
 type Config struct {
-	startUrl string
-	urlParsingRules []URLParsingRules
-	dataParsingRules []DataParsingRule
+	StartUrl string
+	UrlParsingRules URLParsingRules
+	DataParsingRules []DataParsingRule
 }
 
 /* Rules for parsing urls */
 type URLParsingRules struct {
-	sameDomain bool
+	SameDomain bool
 }
 
 /* rules for parsing data from the DOM */
 type DataParsingRule struct {
 	/* url pattern to match the rule to */
-	urlMatch string
+	UrlMatch string
 	/* the data goquery selector string (e.g. "div.content > div#main p.text") */
-	dataSelector string
+	DataSelector string
 }
 
 /* Default URL Parsing rules */
 func NewURLParsingRules() URLParsingRules {
-	return URLParsingRules{sameDomain: true}
+	return URLParsingRules{SameDomain: true}
 }
 /* Initialize URL Parsing rules defined by the user */
 func InitURLParsingRules(sameDomain bool) URLParsingRules {
-	return URLParsingRules{sameDomain: sameDomain}
+	return URLParsingRules{SameDomain: sameDomain}
 }
 
 func ReadConfig(path string) Config {
-	var config Config
 	/* check if the file exists */
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal("Invalid path to the configuration file: ", path)
 	}
-	json.Unmarshal(file, &config)
+	return readConfig(file)
+}
+
+func readConfig(contents []byte) Config {
+	var config Config
+	err := json.Unmarshal(contents, &config)
+	if err != nil {
+		log.Fatal("Unable to unmarshal the configuration file into an object: ", string(contents))
+	}
 	return config
 }
