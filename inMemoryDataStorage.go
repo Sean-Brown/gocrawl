ce0ec76da2ce86ea25b8a782199c17ffa695a764
Sean Brown <smbrown3706@gmail.com>
2016-09-21 11:54:37 -0400

    Updated the Store() signature to match the interface and added a map of <url, data>

--- inMemoryDataStorage.go
+++ inMemoryDataStorage.go
@@ -5,8 +5,11 @@ import "log"
 type InMemoryDataStorage struct {
 	/* Implement the DataStorage interface */
 	DataStorage
+	/* Store data in a map of <url, data> */
+	ds map[string]string
 }
 
-func (storage *InMemoryDataStorage) Store(data string) {
-	log.Println("Storing data: ", data)
+func (storage *InMemoryDataStorage) Store(url string, data string) {
+	log.Println("Received data for url: ", url, ", data: ", data)
+	storage.ds[url] = data
 }
