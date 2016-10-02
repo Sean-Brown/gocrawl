dc46273ab6e04c5e19bb6023b0c2985c7a6226a6
Sean Brown <smbrown3706@gmail.com>
2016-10-02 17:15:41 -0400

    added the testify/assert library for doing test assertions. Added an implementation of DataStorage that tracks how many times a url was stored: this helps to identify if urls are being crawled twice

--- tests/urlConsumer_test.go
+++ tests/urlConsumer_test.go
@@ -8,19 +8,54 @@ import (
 	"testing"
 	"github.com/Sean-Brown/gocrawl/gocrawl"
 	"github.com/Sean-Brown/gocrawl/config"
+	"github.com/stretchr/testify/assert"
 	"strings"
 )
 
-func getConfig(host string, page string, sameDomain bool, depth int, dataSelector string) config.Config {
-	return config.Config{
-		StartUrl: fmt.Sprintf("http://%s:%d/%s.html", host, 2015, page),
+type CountingDS struct {
+	config.DataStorage
+	// Map of url to # of times it was written to
+	store map[string] int
+}
+func (ds *CountingDS) Store(url string, data string) {
+	ds.store[url]++
+}
+func (ds *CountingDS) Get(url string) string {
+	return fmt.Sprint(ds.store[url])
+}
+func (ds *CountingDS) NumItems() int {
+	return len(ds.store)
+}
+func NewCountingDS() *CountingDS {
+	return &CountingDS{store: make(map[string]int)}
+}
+
+/**
+ Return a string formatted as http://<host>:2015/<page>.html
+ */
+func formatPage(host string, page string) string {
+	return fmt.Sprintf("http://%s:%d/%s.html", host, 2015, page)
+}
+
+func getConfig(inMemory bool, host string, page string, sameDomain bool, depth int, dataSelectors []string) config.Config {
+	var dataParsingRules []config.DataParsingRule
+	for _, selector := range dataSelectors {
+		dataParsingRules = append(dataParsingRules, config.DataParsingRule{
+			UrlMatch: "host.*",
+			DataSelector: selector,
+		})
+	}
+	var ds config.DataStorage
+	if inMemory {
+		ds = gocrawl.CreateInMemoryDataStore()
+	} else {
+		ds = NewCountingDS()
+	}
+	return config.Config {
+		StartUrl: formatPage(host, page),
 		UrlParsingRules: config.CreateURLParsingRules(sameDomain, depth),
-		DataParsingRules: []config.DataParsingRule{
-			{
-				UrlMatch:"host.*",
-				DataSelector:dataSelector,
-			},
-		},
+		DataParsingRules: dataParsingRules,
+		DataStore: ds,
 	}
 }
 
@@ -81,24 +116,67 @@ func dataAreEqual(actual string, expected string) bool {
 	return strings.Compare(actual, expected) == 0
 }
 
-func Test_HostA_Page1_SameDomain_Depth1(t *testing.T) {
+func expectData(t *testing.T, crawler *gocrawl.GoCrawl, url string, expected string) {
+	data := (*crawler).GetDS().Get(url)
+	assert.True(t, dataAreEqual(data, expected), fmt.Sprintf("%s != %s", data, expected))
+}
+
+func DISABLED_Test_HostA_Page1_SameDomain_Depth1(t *testing.T) {
 	/* run the test */
 	crawler, quit, wait := runTest(getConfig(
+		true,
 		"hosta",
 		"page1",
 		true,
 		1,
-		"p#data",
+		[]string{"p#data"},
 	))
 
 	if crawler != nil {
 		/* assert that we got the data we expect to */
-		ds := (*crawler).GetDS()
-		data := ds.Get("http://hosta:2015/page1.html")
-		expected := "Page 1 Data"
-		if !dataAreEqual(data, expected) {
-			t.Fatal(data, " != ", expected)
-		}
+		expectData(t, crawler, formatPage("hosta", "page1"), "Page 1 Data")
+	}
+
+	/* end the test */
+	endTest(quit, wait)
+}
+
+func Test_HostA_Page1_SameDomain_Depth2(t *testing.T) {
+	/* run the test */
+	crawler, quit, wait := runTest(getConfig(
+		true,
+		"hosta",
+		"page1",
+		true,
+		2,
+		[]string{"p#data", "div#data", "div#ultra-cool p.data"},
+	))
+
+	if crawler != nil {
+		/* assert that we got the data we expect to */
+		expectData(t, crawler, formatPage("hosta", "page1"), "Page 1 Data")
+		expectData(t, crawler, formatPage("hosta", "page2"), "Page 2 Data")
+		expectData(t, crawler, formatPage("hosta", "page3"), "Page 3 Data")
+	}
+
+	/* end the test */
+	endTest(quit, wait)
+}
+
+func Test_DoesNotCrawlSamePageTwice(t *testing.T) {
+	/* run the test */
+	crawler, quit, wait := runTest(getConfig(
+		false,
+		"hosta",
+		"page1",
+		true,
+		2,
+		[]string{"p#data", "div#data", "div#ultra-cool p.data"},
+	))
+
+	if crawler != nil {
+		/* assert that hosta was only crawled once */
+		assert.Equal(t, crawler.GetDS().Get(formatPage("hosta", "page1")), "1")
 	}
 
 	/* end the test */
