e247516dc717857830b26b9ecbf1488bd67ee6b1
Sean Brown <smbrown3706@gmail.com>
2016-10-03 21:16:30 -0400

    Removed the 'timeout' in favor of data tracking: only quit if there is no more data in the queues and if there are no active consumers

--- gocrawl/gocrawl.go
+++ gocrawl/gocrawl.go
@@ -13,13 +13,10 @@ type GoCrawl struct {
 	dataConsumer *DataConsumer
 	/* The data storage */
 	dataStore config.DataStorage
-	/* Timeout -- the time (in seconds) to wait with empty channels, if this time elapses,
-	then the crawler will assume there are no more urls or data left and will thus exit*/
-	timeout int
 }
 
 func NewGoCrawl() GoCrawl {
-	return GoCrawl{timeout: 2}
+	return GoCrawl{}
 }
 
 func (crawler *GoCrawl) GetDS() config.DataStorage {
@@ -44,7 +41,6 @@ func (crawler *GoCrawl) Crawl(crawlConfig config.Config, quit chan int, done cha
 	go crawler.dataConsumer.Consume()
 
 	/* Wait until signalled to quit or until there are no more URLs and data */
-	elapsed := time.Now()
 loop:
 	for {
 		select {
@@ -54,14 +50,10 @@ loop:
 		default:
 			// are there no more urls and data?
 			noData := len(urls) == 0 && len(data) == 0
-			timeElapsed := (int)(time.Now().Sub(elapsed).Seconds())
-			timedOut := (timeElapsed > crawler.timeout)
-			if noData && timedOut {
+			noWorkers := crawler.dataConsumer.NumWorkers() == 0 && crawler.urlConsumer.NumWorkers() == 0
+			if noData && noWorkers {
 				// no more urls or data, break out of the loop
 				break loop
-			} else if (!noData) {
-				// there is still data in the channels, reset the elapsed timer
-				elapsed = time.Now()
 			}
 			// sleep for one second before continuing
 			time.Sleep(1 * time.Second)
