084a2d6d808a722b317f282b0a4457ea8604163c
Sean Brown <smbrown3706@gmail.com>
2016-10-03 21:12:31 -0400

    Added worker tracking so that consumer threads won't be prematurely quit.

--- gocrawl/consumer.go
+++ gocrawl/consumer.go
@@ -4,6 +4,9 @@ import "sync"
 
 type iConsumer interface {
 	Consume()
+	IncWorkers()
+	DecWorkers()
+	NumWorkers() int
 }
 
 type Consumer struct {
@@ -13,4 +16,26 @@ type Consumer struct {
 	Quit chan int
 	/* waitGroup to signal to the main service when the consumer thread has stopped */
 	WaitGroup *sync.WaitGroup
+	/* the number of threads working */
+	numWorkers int
+	/* mutex for interacting with numWorkers */
+	workerMux sync.RWMutex
+}
+
+func (consumer *Consumer) IncWorkers() {
+	consumer.workerMux.Lock()
+	consumer.numWorkers++
+	consumer.workerMux.Unlock()
+}
+func (consumer *Consumer) DecWorkers() {
+	consumer.workerMux.Lock()
+	consumer.numWorkers--
+	consumer.workerMux.Unlock()
+}
+func (consumer *Consumer) NumWorkers() int {
+	var numWorkers int
+	consumer.workerMux.RLock()
+	numWorkers = consumer.numWorkers
+	consumer.workerMux.RUnlock()
+	return numWorkers
 }
